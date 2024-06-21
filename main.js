9d9b126e14dc16c60b0a02b0b83c94817f565fed
823Njoroge <njorogeian823@gmail.com>
2024-06-21 13:57:54 +0300

    ADDED MORE REPONSIVENSESS IN JS AND CSS

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend dev", "YouTube", "Web developer"],
+  strings: ["Frontend dev", "Web developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
+  loop: true,
 });
