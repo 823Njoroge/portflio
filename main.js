4a3fe9ffaec2f88259130f2ae16afba2a4415e7d
823Njoroge <njorogeian823@gmail.com>
2024-06-29 13:35:52 +0300

    added js section

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend dev", "Web developer"],
+  strings: ["Frontend developer", "Data Scientist"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
